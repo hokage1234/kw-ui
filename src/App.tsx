e9959250daf8149187f6cee2a0c7e4430064cd85
DK <kowszewiczdawid@gmail.com>
2025-06-23 19:35:14 +0200

    Drugi commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> Siema Daniel .
+          Edit <code>src/App.tsx</code> Cześć Daniel dzięki za pomoc.
         </p>
         <a
           className="App-link"
